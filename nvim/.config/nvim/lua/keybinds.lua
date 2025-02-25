85e8b6b481be87434c6c7f923e039db0de6c2875
caeklol <me@caek.dev>
2025-02-25 23:12:08 +0800

    nvim: add code actions keybind

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = "\\"
 
 -- primeagen
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- code actions
+vim.keymap.set('n', '<leader>ca', function()
+	vim.lsp.buf.code_action({apply=true})
+end, bufopts)
