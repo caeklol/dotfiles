b24ee8bc6160fc143a88f576aa19c5444eeae96f
caeklol <me@caek.dev>
2025-06-04 19:24:26 +0800

    nvim: switch to lazy.nvim [1]

---
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,9 @@
+return {
+	{
+		'nvim-telescope/telescope.nvim', tag = '0.1.8',
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		keys = { 
+				{ "<leader><leader>", "<cmd>Telescope find_files<CR>" },
+			},
+	}
+}
